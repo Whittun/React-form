df9f653be83e0f44595d7d670304ac4a0e5100f2
Whittun <whittun@gmail.com>
2024-11-15 15:10:26 +0300

    feat: add the first version of the scheme and field output

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import "./App.css";
-import { z } from "zod";
-
-const schema = z.object({
-  name: z.string().min(4, `Должно содержать не менее 4 символов`),
-  age: z.number({ message: "Нужно указать число" }),
-  email: z.string().email("Нужно указать почту"),
-  phoneNumber: z.number({ message: "Нужно указать номер телефона" }),
-  childName: z.string().min(4, `Должно содержать не менее 4 символов`),
-  childAge: z.number({ message: "Нужно указать число" }),
-});
-
-console.log(schema.parse({ name: "anam", age: "s" }));
+import { Form } from "./components/Form";
 
 function App() {
-  return <></>;
+  return <Form />;
 }
 
 export default App;
